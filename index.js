d968c2301ac28469c5b1cd5fd7ffb9831cf7a1b7
Alabhya Jindal <alabhya10@gmail.com>
2024-10-10 17:29:11 +0100

    add ability to enter decimals, operators and clear the screen via the keyboard

--- index.js
+++ index.js
@@ -21,10 +21,15 @@ function calculate() {
 document.addEventListener('keydown', (e) => {
   const num = Number(e.key)
   if (num >= 0 && num <= 9) {
-    console.log(e.key)
+    appendToDisplay(e.key)
+  } else if (e.key == '.') {
     appendToDisplay(e.key)
   } else if (e.key == 'Enter') {
     document.activeElement.blur()
     calculate()
+  } else if (e.key == '*' || e.key == '/' || e.key == '+' || e.key == '-') {
+    appendToDisplay(e.key)
+  } else if (e.key.toLowerCase() == 'c') {
+    clearDisplay()
   }
 })
